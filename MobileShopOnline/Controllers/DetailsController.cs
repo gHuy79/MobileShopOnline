a0e5f071ff0cd57242a9058ed2211a17edad8482
ADMIN <nguyenngogiahuy79@gmail.com>
2023-10-26 08:53:29 +0700

    Add project files.

---
+++ MobileShopOnline/Controllers/DetailsController.cs
@@ -0,0 +1,30 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using MobileShopOnline.Models;
+using System.Data.Entity;
+
+namespace MobileShopOnline.Controllers
+{
+    public class DetailsController : Controller
+    {
+        MobileShopOnlineEntities db = new MobileShopOnlineEntities();
+        public ActionResult Index(int id)
+        {
+            
+
+            ViewBag.ProdDetails = db.Products.FirstOrDefault(n => n.ProductID == id);
+            int thisProdCategories = db.Products.FirstOrDefault(n => n.ProductID == id).CategoryID;
+
+            ViewBag.ThisProdCategories = db.Categories.FirstOrDefault(n => n.CategoryID == thisProdCategories);
+
+            ViewBag.ProductList = (from p in db.Products where p.CategoryID == thisProdCategories && p.ProductID != id select p).ToList().Take(10);
+            
+            
+            return View();
+        }
+    }
+}
